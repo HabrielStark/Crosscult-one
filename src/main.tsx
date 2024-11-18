12de1b374fc28cff21bedc3b69d4d131bb01240d
HabrielStark <gabrielldubov12@gmail.com>
2024-11-18 18:31:37 +0100

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { ThemeProvider } from './hooks/use-theme'
+import { ThemeProvider } from './components/theme-provider'
 import './app/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
